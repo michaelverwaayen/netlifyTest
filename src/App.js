aef7b2163676e605ecac2ea73f1bf944db0640dc
michaelverwaayen <mverwaayen@outlook.com>
2019-03-29 10:56:47 -0600

    b

--- src/App.js
+++ src/App.js
@@ -3,38 +3,113 @@ import logo from './logo.svg';
 import './App.css';
 import S3FileUpload from 'react-s3';
 import ReactS3 from 'react-s3';
+var aws = require('aws-sdk'); 
+import axios from 'axios';
  
-const config = {
-  bucketName: 'imageupload23',
-
+aws.config.update({
   region: 'us-east-1',
   accessKeyId: 'AKIAIS7CFAA7EYZF5FIA',
   secretAccessKey: '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',
+});
+const S3_BUCKET = 'michaelverwaayen1';
+
+exports.sign_s3 = (req,res) => {
+  const s3 = new aws.S3();  // Create a new instance of S3
+  const fileName = req.body.fileName;
+  const fileType = req.body.fileType;
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 500,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      res.json({success: false, error: err})
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+    // Send it all back
+    res.json({success:true, data:{returnData}});
+  });
 }
 
 class App extends Component {
-  upload(e){
-    console.log(e.target.files[0]);
-    ReactS3
-  .uploadFile(e.target.files[0], config)
-  .then(data => console.log(data))
-  .catch(err => console.error(err))
+  constructor(props){
+    super(props);
+    this.state = {
+      success : false,
+      url : ""
+    }
   }
   
-render () {
-
-  return (
-      <div> 
-        <h3>
-          upload Image
-        </h3>
-        <input type= "file"
-          onChange = {this.upload}/>
-        confirmed working
+  handleChange = (ev) => {
+    this.setState({success: false, url : ""});
+    
+  }
+  // Perform the upload
+  handleUpload = (ev) => {
+    let file = this.uploadInput.files[0];
+    // Split the filename to get the name and type
+    let fileParts = this.uploadInput.files[0].name.split('.');
+    let fileName = fileParts[0];
+    let fileType = fileParts[1];
+    console.log("Preparing the upload");
+    axios.post("http://localhost:3001/sign_s3",{
+      fileName : fileName,
+      fileType : fileType
+    })
+    .then(response => {
+      var returnData = response.data.data.returnData;
+      var signedRequest = returnData.signedRequest;
+      var url = returnData.url;
+      this.setState({url: url})
+      console.log("Recieved a signed request " + signedRequest);
+      
+     // Put the fileType in the headers for the upload
+      var options = {
+        headers: {
+          'Content-Type': fileType
+        }
+      };
+      axios.put(signedRequest,file,options)
+      .then(result => {
+        console.log("Response from s3")
+        this.setState({success: true});
+      })
+      .catch(error => {
+        alert("ERROR " + JSON.stringify(error));
+      })
+    })
+    .catch(error => {
+      alert(JSON.stringify(error));
+    })
+  }
+  
+  
+  render() {
+    const Success_message = () => (
+      <div style={{padding:50}}>
+        <h3 style={{color: 'green'}}>SUCCESSFUL UPLOAD</h3>
+        <a href={this.state.url}>Access the file here</a>
+        <br/>
       </div>
-  );
-
+    )
+    return (
+      <div className="App">
+        <center>
+          <h1>UPLOAD A FILE</h1>
+          {this.state.success ? <Success_message/> : null}
+          <input onChange={this.handleChange} ref={(ref) => { this.uploadInput = ref; }} type="file"/>
+          <br/>
+          <button onClick={this.handleUpload}>UPLOAD</button>
+        </center>
+      </div>
+    );
+  }
 }
-}
-
 export default App;
